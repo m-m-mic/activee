de2440b1d3501c863b34b15b5c2fbc716330585f
Michael <michael.j.buchholz@t-online.de>
2022-12-12 20:50:41 +0100

    adjusted responsive behavior of horizontal buttons

--- src/components/HorizontalButton.js
+++ src/components/HorizontalButton.js
@@ -3,22 +3,11 @@ import "../assets/css/HorizontalButton.css";
 import DeleteIconWhite from "../assets/svgs/delete_icon_white.svg";
 
 export function HorizontalButton({ iconUrl, children, editMode, onClick, value }) {
-  if (editMode) {
-    return (
-      <span className="horizontal-button-edit-mode">
-        <span className="horizontal-button-data">
-          <img className="horizontal-button-icon" src={iconUrl} alt="button icon" />
-          <div className="horizontal-button-title">{children}</div>
-        </span>
-        <img onClick={onClick} className="horizontal-delete-icon" src={DeleteIconWhite} alt="button icon" />
-      </span>
-    );
-  } else {
-    return (
-      <span className="horizontal-button">
-        <img className="horizontal-button-icon" src={iconUrl} alt="button icon" />
-        <div className="horizontal-button-title">{children}</div>
-      </span>
-    );
-  }
+  return (
+    <span className="horizontal-button">
+      <img className="horizontal-button-icon" src={iconUrl} alt="button icon" />
+      <div className="horizontal-button-title">{children}</div>
+      {editMode && <img onClick={onClick} className="horizontal-delete-icon" src={DeleteIconWhite} alt="button icon" />}
+    </span>
+  );
 }
