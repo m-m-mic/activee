998d037bf7f5f6e38d4335fec4850c21cd9d4eb6
Michael <michael.j.buchholz@t-online.de>
2022-12-19 10:45:54 +0100

    now responds to wrong email and password response from backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,15 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { ActiveeButton } from "../components/ActiveeButton";
 import "../assets/css/Login.css";
 import { useCookies } from "react-cookie";
+import { WarningDisclaimer } from "../components/WarningDisclaimer";
 
 export function Login() {
   const navigate = useNavigate();
   const [cookies, setCookie] = useCookies(["userToken", "userId", "userType"]);
   const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
+  const [wrongEmailDisclaimerVisible, setWrongEmailDisclaimerVisible] = useState(false);
+  const [wrongPasswordDisclaimerVisible, setWrongPasswordDisclaimerVisible] = useState(false);
   useEffect(() => {
     document.title = "Anmelden - activee";
   });
@@ -19,10 +22,21 @@ export function Login() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email: email, password: password }),
     };
-    fetch(url, requestOptions)
-      .then((response) => response.json())
-      .then((data) => handleCookies(data.token, data.id, data.type, data.tier))
-      .then(() => navigate("/"));
+    fetch(url, requestOptions).then((response) => {
+      if (response.status === 404) {
+        setWrongEmailDisclaimerVisible(true);
+        setWrongPasswordDisclaimerVisible(false);
+        return;
+      } else if (response.status === 403) {
+        setWrongEmailDisclaimerVisible(false);
+        setWrongPasswordDisclaimerVisible(true);
+        return;
+      }
+      response
+        .json()
+        .then((data) => handleCookies(data.token, data.id, data.type, data.tier))
+        .then(() => navigate("/"));
+    });
   };
   const handleCookies = (token, userId, userType, userTier) => {
     setCookie("userToken", token, {
@@ -42,6 +56,12 @@ export function Login() {
     <>
       <div className="login-hero">Willkommen zurück</div>
       <div className="login-wrapper">
+        <WarningDisclaimer isDisclaimerVisible={wrongEmailDisclaimerVisible}>
+          E-Mail konnte nicht gefunden werden
+        </WarningDisclaimer>
+        <WarningDisclaimer isDisclaimerVisible={wrongPasswordDisclaimerVisible}>
+          Es wurde ein falsches Password eingegeben
+        </WarningDisclaimer>
         <div className="login-input-wrapper">
           <input
             value={emailInput}
